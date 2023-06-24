ccc9318ad530f7c122dcd57a7bb585dfe94c9a87
Troy Connor <troy0820@users.noreply.github.com>
2023-06-24 11:06:01 -0400

    add client-go scheme to clientset
    Signed-off-by: Troy Connor <troy0820@users.noreply.github.com>

--- clientset/v1/porterclientset/fake/fake_porterclientset.go
+++ clientset/v1/porterclientset/fake/fake_porterclientset.go
@@ -3,6 +3,7 @@ package fake
 import (
 	v1 "get.porter.sh/operator/api/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 )
@@ -10,6 +11,7 @@ import (
 func NewFakePorterClientSetWithObjects(objs ...runtime.Object) client.WithWatch {
 	scheme := runtime.NewScheme()
 	v1.AddToScheme(scheme)
+	clientgoscheme.AddToScheme(scheme)
 	fakePorterClient := fake.NewClientBuilder().WithScheme(scheme).WithRuntimeObjects(objs...).Build()
 	return fakePorterClient
 }
@@ -17,6 +19,7 @@ func NewFakePorterClientSetWithObjects(objs ...runtime.Object) client.WithWatch 
 func NewFakePorterClient() client.WithWatch {
 	scheme := runtime.NewScheme()
 	v1.AddToScheme(scheme)
+	clientgoscheme.AddToScheme(scheme)
 	fakePorterClient := fake.NewClientBuilder().WithScheme(scheme).Build()
 	return fakePorterClient
 }
--- clientset/v1/porterclientset/porterclientset.go
+++ clientset/v1/porterclientset/porterclientset.go
@@ -3,6 +3,7 @@ package porterclientset
 import (
 	v1 "get.porter.sh/operator/api/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -10,6 +11,7 @@ import (
 func NewPorterClientSet(config *rest.Config) (client.WithWatch, error) {
 	scheme := runtime.NewScheme()
 	v1.AddToScheme(scheme)
+	clientgoscheme.AddToScheme(scheme)
 	porterClient, err := client.NewWithWatch(config, client.Options{Scheme: scheme})
 	if err != nil {
 		return nil, err
